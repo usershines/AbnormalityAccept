d4416908f2b49cf64b461ca7959547abcaa2e8b2
usershines <161181988+usershines@users.noreply.github.com>
2025-07-13 14:05:02 +0800

    Add files via upload

---
+++ abnormalityAccept/src/main/java/com/abnormality/abnormalityaccept/AbnormalityAcceptApplication.java
@@ -0,0 +1,13 @@
+package com.abnormality.abnormalityaccept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbnormalityAcceptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbnormalityAcceptApplication.class, args);
+    }
+
+}
---
+++ abnormalityAccept/src/test/java/com/abnormality/abnormalityaccept/AbnormalityAcceptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abnormality.abnormalityaccept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbnormalityAcceptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

46f5ba54a2af3d43dd37adedb953dd4120b053e8
poyuan <poyuan233666@163.com>
2025-07-13 19:50:54 +0800

    更新Aspect样例

--- abnormalityAccept/src/main/java/com/abnormality/abnormalityaccept/aspect/LogAspect.java
+++ abnormalityAccept/src/main/java/com/abnormality/abnormalityaccept/aspect/LogAspect.java
@@ -1,4 +1,36 @@
 package com.abnormality.abnormalityaccept.aspect;
 
+import com.abnormality.abnormalityaccept.dto.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
 public class LogAspect {
+
+
+    @Autowired
+    private ApplicationContext applicationContext;
+    @Pointcut("execution(* com.abnormality.abnormalityaccept.controller.*.*(..))")
+    public void log(){
+
+    }
+
+    @Around("log()")
+    public Result<?> controller(ProceedingJoinPoint joinPoint)  {
+        try{
+            Result result= (Result) joinPoint.proceed();
+            log.info("{}",result);
+            return result;
+        } catch (Throwable e) {
+            return null;
+        }
+    }
 }

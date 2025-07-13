24005db521864bada254f93148b43bd34ea90a0b
usershines <shanhai20041107@outlook.com>
2025-07-13 19:40:09 +0800

    user增删改查

--- abnormalityAccept/src/main/java/com/abnormality/abnormalityaccept/entity/User.java
+++ abnormalityAccept/src/main/java/com/abnormality/abnormalityaccept/entity/User.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Schema(name = "用户信息")
 public class User {
 
     private Integer id;
--- abnormalityAccept/src/main/java/com/abnormality/abnormalityaccept/service/UserService.java
+++ abnormalityAccept/src/main/java/com/abnormality/abnormalityaccept/service/UserService.java
@@ -1,10 +1,39 @@
 package com.abnormality.abnormalityaccept.service;
 
+import com.abnormality.abnormalityaccept.entity.User;
+import com.github.pagehelper.PageInfo;
+import org.springframework.stereotype.Service;
+
 /**
  * @author shanh
  * @version 1.0
  * {@code @description:}
  * @since 2025-07-13
  */
+@Service
 public interface UserService {
+    /**
+     *查询所有用户
+     */
+    PageInfo<User> findAllUser(Integer pageNum, Integer pageSize);
+    /**
+     *根据id查询用户
+     */
+    User findUserById(Integer id);
+
+    /**
+     * 删除数据
+     */
+    boolean deleteUserById(Integer id);
+
+    /**
+     * 新增数据
+     */
+    boolean addUser(User user);
+
+    /**
+     * 修改数据
+     */
+    boolean updateUser(User user);
+
 }

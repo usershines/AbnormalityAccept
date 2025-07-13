eec5eab40335f3566d1cb67402277708c8534ddc
poyuan <poyuan233666@163.com>
2025-07-13 16:22:01 +0800

    更改目录结构

--- abnormalityAccept/src/abnormalityaccept/service/questService.java
+++
@@ -1,9 +0,0 @@
-package com.abnormality.abnormalityaccept.service;
-
-/**
- * @author shanh
- * @version 1.0
- * {@code @description:}
- * @since 2025-07-13
- */public interface questService {
-}
---
+++ abnormalityAccept/src/main/java/com/abnormality/abnormalityaccept/service/userService.java
@@ -0,0 +1,10 @@
+package com.abnormality.abnormalityaccept.service;
+
+/**
+ * @author shanh
+ * @version 1.0
+ * {@code @description:}
+ * @since 2025-07-13
+ */
+public interface userService {
+}
